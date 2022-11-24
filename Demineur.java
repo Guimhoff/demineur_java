8f0370a4b0de61aa4ebee1089ae66b28dac4653e
Guillaume Imhoff <guillaume.imhoff@gmail.com>
2022-11-24 14:52:28 +0100

    Ajout d'une icone

--- Demineur.java
+++ Demineur.java
@@ -1,3 +1,5 @@
+import javax.swing.ImageIcon;
+
 import app.View.Window;
 
 public class Demineur {
@@ -10,6 +12,10 @@ public class Demineur {
   public static void main(String argv[]) {
     
     Window f = new Window();
+    
+    ImageIcon icon = new ImageIcon("images/bomb.png");
+    f.setIconImage(icon.getImage());
+    
     f.setVisible(true);
   }
   
